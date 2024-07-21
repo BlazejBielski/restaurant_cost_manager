4550932ea3c5ae25f0a6013490f2b0889c726de3
blazej <bbielski@lyck.pl>
2024-07-21 20:29:14 +0200

    fix settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third part
-    "djangorestframework",
+    "rest_framework",
     # app
     "users.apps.UsersConfig",
 ]
@@ -126,4 +126,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.CustomUser"
