79cf16cbfa40e9ee7994301028866ab152fb08d6
blazej <bbielski@lyck.pl>
2024-07-22 20:16:27 +0200

    seializer

---
+++ backend/users/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'username')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'required': True},
+            'username': {'read_only': True},
+        }
