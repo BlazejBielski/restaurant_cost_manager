6836b9bf9a000d7b8f26088b30f4e43f0cd61055
blazej <bbielski@lyck.pl>
2024-07-22 20:27:11 +0200

    views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status, permissions
+from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from users import serializers
+from users.serializers import CustomUserSerializer
+
+
+class CreateCustomUserView(CreateAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = get_user_model().objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = serializers.CustomUserSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GetCustomUserView(RetrieveAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = serializers.CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
